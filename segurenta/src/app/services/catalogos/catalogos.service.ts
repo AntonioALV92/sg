9144ce65e8a344c8ffa28eb40ded0c948e5a03dd
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2019-04-01 13:19:27 -0600

    se agregan los endpoints al archivo config

--- segurenta/src/app/services/catalogos/catalogos.service.ts
+++ segurenta/src/app/services/catalogos/catalogos.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { MiddlewareService } from '../middleware/middleware.service'
-import { ConfigurationService } from '../configuration/configuration.service'
+import { MiddlewareService } from '../middleware/middleware.service';
+import { ConfigurationService } from '../configuration/configuration.service';
 import { error } from '@angular/compiler/src/util';
 
 @Injectable()
@@ -10,32 +10,14 @@ export class CatalogosService {
 
   public getBancos() {
     return new Promise((resolve) => {
-      this.middleware.get(this.config.endpoints.getBancos).subscribe((res: any) => {
-        return resolve(res);
-        // let response = JSON.parse(JSON.stringify(data));
-        // if (response.codE === 0) {
-        //     return resolve(response);
-        // } else {
-        //     /*Notifications*/
-        //     return resolve(response);
-        // }
+      this.middleware.get(this.config.endpoints.catBancos).subscribe(
+        (res: any) => {
+          return resolve(res);
+        },
+        (err: any) => {
+          console.error(err);
+        }
+      );
     });
-})
-    // let result =  await this.middleware.get(this.config.endpoints.getBancos).toPromise()
-    // .then(res => {
-    //   return res;
-    // })
-    // .catch (error => {
-    //   console.error(error);
-    // })
-    // .subscribe(
-    //   res => {
-    //     debugger;
-    //     return res;
-    //   },
-    //   error  =>{
-    //     console.log(error);
-    //   }
-    // );
   }
 }
