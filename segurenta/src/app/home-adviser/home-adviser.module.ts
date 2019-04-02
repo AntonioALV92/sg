5aaeed431d8b1e7d2c0a67bb7436fe23913b2489
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2019-04-02 17:24:55 -0600

    restructura proyecto v3

--- segurenta/src/app/home-adviser/home-adviser.module.ts
+++ segurenta/src/app/home-adviser/home-adviser.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 import { HomeAdviserComponent } from 'src/app/home-adviser/home-adviser.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { RequestDateComponent } from './components/request-date/request-date.component';
+import { InProgressComponent } from './components/in-progress/in-progress.component';
+import { MyPropertyComponent } from './components/my-property/my-property.component';
 
 @NgModule({
   declarations: [
     HomeAdviserComponent,
-    MenuComponent
+    MenuComponent,
+    RequestDateComponent,
+    InProgressComponent,
+    MyPropertyComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class HomeAdviserModule { }
