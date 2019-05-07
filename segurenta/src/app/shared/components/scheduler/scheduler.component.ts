fa4a402a677956aa057a84b832a9aa73a7e3208a
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2019-05-07 11:33:11 -0500

    Calendario

--- segurenta/src/app/shared/components/scheduler/scheduler.component.ts
+++ segurenta/src/app/shared/components/scheduler/scheduler.component.ts
@@ -1,5 +1,4 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
-// import FlatpickrEs from 'flatpickr/dist/l10n/es';
 import { CalendarioService } from '../../../services/calendario/calendario.service';
 @Component({
   selector: 'app-scheduler',
@@ -10,17 +9,8 @@ import { CalendarioService } from '../../../services/calendario/calendario.servi
 
 export class SchedulerComponent implements OnInit {
 
-  // declare const Flatpickr: any;
-  // const Flatpickr: any;
-
-  // Flatpickr.localize(FlatpickrLanguages.es);
-
   public viewDate: Date;
   public listDates: any;
-  // public event: any;
-  // public view: string;
-  // public markDays: boolean;
-  // public lan: any;
 
   constructor(private service: CalendarioService) {}
 
@@ -32,21 +22,4 @@ export class SchedulerComponent implements OnInit {
   private async getDates(num: number) {
     this.listDates = await this.service.getDates(num);
   }
-
-  // constructor() {
-    // this.Flatpickr.localize(require('assets/i10n/flatpickr.l10n.hr.js'));
-    // console.log(FlatpickrEs);
-    // this.lan = FlatpickrEs;
-    // this.view = 'week';
-    // this.markDays = false;
-  //   flatpickr(myElem, {
-  //     "locale": Russian // locale for this instance only
-  // });
-  // }
-
-  // public setView(view: string) {
-  //   if (this.view.localeCompare(view) !== 0) {
-  //     this.view = view;
-  //   }
-  // }
 }
